73770867faf69ebaefb94a0e5796921507b9d490
DMCatanach <dawn.catanach@gmail.com>
2016-07-11 19:51:50 -0600

    weather app and first sinatra script

--- weather-app/weather.rb
+++ weather-app/weather.rb
@@ -1,24 +1,31 @@
 require 'barometer'
 
 #temperature.f gives temp in Fahrenheit; default is celsius
-#current.condition gives current conditions, .condition doesn't seem to work with .forecast, .today, .tomorrow
+#current.condition gives current conditions, .condition doesn't work with .forecast, .today, .tomorrow
 #can use .high, .low with .today and .tomorrow
-#still don't know what info can get with .forecast
 
-#method to determine weather in user's location
-def get_weather
+#method to determine user's location, called in new barometer later
+def get_location
 	
 	puts "What is your current location?"
 	user_location = gets.chomp
-	barometer = Barometer.new(user_location)
-	weather = barometer.measure
-	current_weather = weather.current.condition.to_s
-	puts "Right now, it is #{current_weather.downcase}."
 	
 end
 
-#get_weather
-
-barometer = Barometer.new('Albuquerque')
-weather = barometer.measure
-puts weather.inspect
+#basic forecast, calls get_location method for location data
+#now uses strftime to display forecast with "tomorrow" and days of the week. 
+barometer = Barometer.new(get_location)
+weather = barometer.measure  #gets measurements; vital step! 
+tomorrow = Time.now.strftime('%d').to_i + 1  #this provides comparison for if/else loop; if using %w, will just put day of week
+weather.forecast.each do |forecast| 
+	day = forecast.starts_at.day
+	#determining whether to put "tomorrow" or weekday name in forecast output 
+	if day == tomorrow 
+		weekday = "Tomorrow"
+	else 
+		weekday = forecast.starts_at.strftime('%A')
+	end
+	#forecast output 
+	puts "#{weekday} is going to be #{forecast.icon} with a low of #{forecast.low.f.to_s} and a high of #{forecast.high.f.to_s}."
+				
+end
--- weather_app.rb
+++ weather_app.rb
@@ -2,4 +2,10 @@
 #step 1: user inputs location - zipcode, city, landmark, Woe ID (what's that?)
 #step 2: use public api to find out weather in that area; Barometer gem
 #step 3: serve up a screen depending on weather 
+#above steps all included in step 1 lesson resource 
+
 #step 4: turn it into forecasting app 
+#this is on lesson resource step 2 
+#first, show forecast for next five days. loop through forecast (review lesson 13)
+#method in barometer gem to return array of future weather conditions? 
+#inspect instance of gem to find methods it has 
