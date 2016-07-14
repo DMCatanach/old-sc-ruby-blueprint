a70b014dac9ef2a153f5b16850b8d108010efaaa
DMCatanach <dawn.catanach@gmail.com>
2016-07-13 19:12:16 -0600

    Refactored numerology script and fixed bug for certain birthdates

--- numerology2.rb
+++ numerology2.rb
@@ -1,29 +1,19 @@
 def get_birthdate
 	print "When is your birthday(format: MMDDYYYY)?"
-	birthday = gets.chop
+	birthdate = gets.chop
 end
 
-def determine_path_number
-	birthdate = get_birthdate.split ""
-	bd1 = birthdate[0].to_i
-	bd2 = birthdate[1].to_i
-	bd3 = birthdate[2].to_i
-	bd4 = birthdate[3].to_i
-	bd5 = birthdate[4].to_i 
-	bd6 = birthdate[5].to_i
-	bd7 = birthdate[6].to_i
-	bd8 = birthdate[7].to_i
-	step1 = bd1+bd2+bd3+bd4+bd5+bd6+bd7+bd8
-	if (step1 > 9)
-		step2 = step1.to_s 
-		step2.split "" 
-		r1 = step2[0].to_i 
-		r2 = step2[1].to_i 
-		path_num = r1+r2 
-	elsif (step1 < 10)
-		path_num = step1
+def determine_path_number(birthdate)
+	path_num = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i  + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
+	if (path_num > 9)
+		path_num = path_num.to_s  #convert to string so it can be split up; initial variable already a string, but here we've converted to integer in the first step
+		path_num = path_num[0].to_i + path_num[1].to_i  
+		if (path_num > 9) #this nested if statement is to account for birthdates that haven't reduced to a single digit after the first step
+			path_num = path_num.to_s
+			path_num = path_num[0].to_i + path_num[1].to_i
+		end
 	end
-	path_num = path_num
+	return path_num
 end
 
 def path_message(path_num)
@@ -31,7 +21,7 @@ def path_message(path_num)
 		when 1
 			message = "One is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
 		when 2
-			message = "This is the mediatore and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+			message = "This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
 		when 3
 			message = "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Threes enjoy life and have a good sense of humor. Ruled by Jupiter."
 		when 4
@@ -53,9 +43,10 @@ def path_message(path_num)
 end
 
 def get_your_path_number
-	your_number = determine_path_number
+	your_number = determine_path_number(get_birthdate)
 	your_message = path_message(your_number)
 	puts "Your number is #{your_number}. #{your_message}"
 end
 
 get_your_path_number
+
