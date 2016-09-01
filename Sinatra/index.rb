51f1793c134d2cccbe06eb58138e6d0f0e766340
DMCatanach <dawn.catanach@gmail.com>
2016-09-01 01:50:34 -0600

    Adds templates for app, working in correct directory

--- Sinatra/index.rb
+++ Sinatra/index.rb
@@ -20,7 +20,7 @@ def path_message(path_num)
 		when 2
 			message = "This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
 		when 3
-			message = "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Threes enjoy life and have a good sense of humor. Ruled by Jupiter."
+			message = "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
 		when 4
 			message = "This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus." 
 		when 5 
@@ -30,9 +30,9 @@ def path_message(path_num)
 		when 7 
 			message = "This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
 		when 8 
-			message = "This is the manager. Number eight is a strong, successful, and material vibration. Ruled by Saturn."
+			message = "This is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
 		when 9
-			message = "This is the teacher. Number nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+			message = "This is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
 		else 
 			message = "You must be exceptional!"
 	end
@@ -48,10 +48,16 @@ end
 get '/' do 
 	"Hello world!"
 end
- 
+
+get '/newpage' do 
+	erb :newpage
+end  
+
 get '/:birthdate' do 
 	birthdate = params['birthdate']
 	path_num = determine_path_number(birthdate)
-	message = path_message(path_num)
-	"#{message}"
+	@message = path_message(path_num)
+	erb :index
 end
+
+
